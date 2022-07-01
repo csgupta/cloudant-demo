11cdd50524223fe963ce41f5dd5e1e7b2b4ac119
Chandershekhar Gupta <chandershekhar.gupta@ibm.com>
2022-07-01 16:59:34 +0530

    change code again

--- src/main/java/socialreview/cloudant/ReviewRestController.java
+++ src/main/java/socialreview/cloudant/ReviewRestController.java
@@ -28,7 +28,7 @@ public class ReviewRestController {
     
     @RequestMapping(method=RequestMethod.GET, path="/hello")
     public ResponseEntity<String> helloWorld(){
-     return ResponseEntity.ok("Hello World IBM TOOLCHAIN");
+     return ResponseEntity.ok("Hello World CODE CHANGE");
     }
 
     // Create a new review

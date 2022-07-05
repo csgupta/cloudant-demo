0c7fd87b4b7fac3daf35a18935e5f8298805fa4f
Chandershekhar Gupta <chandershekhar.gupta@ibm.com>
2022-07-05 10:29:28 +0530

    change code again

--- src/main/java/socialreview/cloudant/ReviewRestController.java
+++ src/main/java/socialreview/cloudant/ReviewRestController.java
@@ -28,7 +28,7 @@ public class ReviewRestController {
     
     @RequestMapping(method=RequestMethod.GET, path="/hello")
     public ResponseEntity<String> helloWorld(){
-     return ResponseEntity.ok("Hello World CODE CHANGE AGAIN");
+     return ResponseEntity.ok("Hello World CODE CHANGE 5th July");
     }
 
     // Create a new review
